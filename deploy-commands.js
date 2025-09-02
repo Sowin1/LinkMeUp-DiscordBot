b8dfcd56e5dfa6819a81c79cfccf2a4a8041592d
Sowin1 <39094589+Sowin1@users.noreply.github.com>
2025-09-02 22:12:01 +0200

    Main code handler bot

---
+++ deploy-commands.js
@@ -0,0 +1,34 @@
+const { REST, Routes } = require('discord.js');
+require('dotenv').config();
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'src', 'commands');
+const commandFolders = fs.readdirSync(commandsPath);
+
+for (const folder of commandFolders) {
+  const folderPath = path.join(commandsPath, folder);
+  const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const filePath = path.join(folderPath, file);
+    const command = require(filePath);
+    commands.push(command.data.toJSON());
+  }
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log(`Déploiement de ${commands.length} commandes (/) en cours...`);
+    const data = await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands },
+    );
+
+    console.log(`✅ ${data.length} commandes (/) ont été rechargées avec succès.`);
+  } catch (error) {
+    console.error(error);
+  }
+})();
---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'src', 'commands');
+const commandFolders = fs.readdirSync(commandsPath);
+
+for (const folder of commandFolders) {
+  const folderPath = path.join(commandsPath, folder);
+  const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const filePath = path.join(folderPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(`[AVERTISSEMENT] La commande à ${filePath} n'a pas les propriétés "data" ou "execute".`);
+    }
+  }
+}
+
+const eventsPath = path.join(__dirname, 'src', 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+
+client.login(process.env.TOKEN);
