7cede8598827e7800c0120b8e5a78bd52271e119
Sowin1 <39094589+Sowin1@users.noreply.github.com>
2025-09-02 23:19:30 +0200

    Implement method to catch message on various guild. Without catching those who are sent in specified Channels/Categories or by a bot

--- index.js
+++ index.js
@@ -36,19 +36,33 @@ for (const folder of commandFolders) {
   }
 }
 
-const eventsPath = path.join(__dirname, "src", "events");
-const eventFiles = fs
-  .readdirSync(eventsPath)
-  .filter((file) => file.endsWith(".js"));
+const eventsPath = path.join(__dirname, 'src', 'events');
 
-for (const file of eventFiles) {
-  const filePath = path.join(eventsPath, file);
-  const event = require(filePath);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args));
+const loadEvents = (dir) => {
+  const files = fs.readdirSync(dir);
+
+  for (const file of files) {
+    const filePath = path.join(dir, file);
+    const stat = fs.lstatSync(filePath);
+    if (stat.isDirectory()) {
+      loadEvents(filePath);
+    } 
+    else if (file.endsWith('.js')) {
+      const event = require(filePath);
+      if (event.name) {
+        if (event.once) {
+          client.once(event.name, (...args) => event.execute(...args));
+        } else {
+          client.on(event.name, (...args) => event.execute(...args));
+        }
+        console.log(`[EVENT] L'événement ${event.name} a été chargé.`);
+      } else {
+        console.log(`[AVERTISSEMENT] Le fichier d'événement à ${filePath} n'a pas de propriété "name".`);
+      }
+    }
   }
-}
+};
+
+loadEvents(eventsPath);
 
 client.login(process.env.TOKEN);
