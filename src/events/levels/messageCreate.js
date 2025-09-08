96731ff0301aef972791a5a9397b5223afbbe33c
Sowin1 <39094589+Sowin1@users.noreply.github.com>
2025-09-09 00:25:39 +0200

    Adding a role when the user reaches a specific level. Giving XP to a user when they bump the guild with Disboard

--- src/events/levels/messageCreate.js
+++ src/events/levels/messageCreate.js
@@ -1,6 +1,7 @@
 const { Events } = require("discord.js");
 const config = require("../../../config.js");
 const db = require("../../database/database.js");
+const DISBOARD_ID = "302050872383242240";
 
 module.exports = {
   name: Events.MessageCreate,
@@ -21,6 +22,35 @@ module.exports = {
     if (!userInDB) {
       db.setUser(user, 0, 0);
       userInDB = db.getUser(user);
+    }
+    if (
+      message.author.id === DISBOARD_ID &&
+      message.interaction &&
+      message.interaction.commandName === "bump" &&
+      message.embeds.length > 0
+    ) {
+      const bumper = message.interaction.user;
+      let bumperInDB = db.getUser(bumper.id);
+      if (!bumperInDB) {
+        db.setUser(bumper.id, 0, 0);
+        bumperInDB = db.getUser(bumper.id);
+      }
+      const newXp = bumperInDB.xp + 20;
+      db.setUser(bumper.id, newXp, bumperInDB.level);
+      let updatedBumperData = db.getUser(bumper.id);
+      let xpNeededForNextLevel = calculateXpForLevel(updatedBumperData.level);
+      while (updatedBumperData.xp >= xpNeededForNextLevel) {
+        const newLevel = updatedBumperData.level + 1;
+        db.setUser(bumper.id, updatedBumperData.xp, newLevel);
+        await message.channel.send(
+          `🎉 Bravo ${bumper}, tu as atteint le niveau **${newLevel}** !`
+        );
+        const guildMember = message.guild.members.cache.get(bumper.id);
+        if (guildMember) givingRole(guildMember, newLevel);
+        updatedBumperData = db.getUser(bumper.id);
+        xpNeededForNextLevel = calculateXpForLevel(updatedBumperData.level);
+      }
+      return;
     }
 
     const lastMessageTime = userInDB.lastMessageTimestamp || 0;
@@ -54,6 +84,7 @@ module.exports = {
       await message.channel.send(
         `🎉 Bravo ${message.author}, tu as atteint le niveau **${newLevel}** !`
       );
+      givingRole(message.member, newLevel);
       updatedUserData = db.getUser(user);
       xpNeededForNextLevel = calculateXpForLevel(updatedUserData.level);
     }
@@ -75,4 +106,21 @@ function calculateXp(charCount) {
 
 function calculateXpForLevel(level) {
   return 5 * Math.pow(level, 2) + 50 * level + 100;
+}
+
+function givingRole(member, level) {
+  const levelRoleMap = {
+    1: config.level1,
+    5: config.level5,
+    10: config.level10,
+    15: config.level15,
+    20: config.level20,
+    25: config.level25,
+    30: config.level30,
+    35: config.level35,
+    40: config.level40,
+  };
+  if (levelRoleMap[level]) {
+    member.roles.add(levelRoleMap[level]);
+  }
 }
