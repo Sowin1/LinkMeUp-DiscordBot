f76f3bb2f0bc8df6709effb3ea8a4ed32ae6175f
Sowin1 <39094589+Sowin1@users.noreply.github.com>
2025-09-09 01:11:20 +0200

    Adding /rank command

---
+++ src/commands/levels/userLevel.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require("discord.js");
+const db = require("../../database/database.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("rank")
+    .setDescription("Affiche le niveau de l'utilisateur")
+    .addUserOption((option) =>
+      option
+        .setName("membre")
+        .setDescription("Le membre dont tu veux voir le niveau")
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    const user = interaction.options.getUser("membre") || interaction.user;
+
+    const userData = db.getUser(user.id);
+
+    if (!userData) {
+      return interaction.reply({
+        content: "Utilisateur non trouvé dans la base de données",
+        ephemeral: true,
+      });
+    }
+
+    interaction.reply({
+      content: `🎖️ ${user} est au niveau **${userData.level}** avec **${userData.xp}** XP`,
+    });
+  },
+};
