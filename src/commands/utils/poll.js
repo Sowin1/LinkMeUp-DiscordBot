1d9495dbb98db3f019d0f422eae62662c8f3dfc4
Timéo Meunier <timecraftyt33@gmail.com>
2025-09-10 23:14:22 +0200

    Adding poll method

--- src/commands/utils/poll.js
+++ src/commands/utils/poll.js
@@ -15,6 +15,24 @@ module.exports = {
         .setDescription("Le sujet du sondage")
         .setRequired(true)
     )
+    .addStringOption((option) =>
+      option
+        .setName("reponse1")
+        .setDescription("La première réponse possible")
+        .setRequired(false)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("reponse2")
+        .setDescription("La deuxième réponse possible")
+        .setRequired(false)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("reponse3")
+        .setDescription("La troisième réponse possible")
+        .setRequired(false)
+    )
     .setDefaultMemberPermissions(PermissionsBitField.Flags.ManageMessages),
 
   async execute(interaction) {
@@ -27,18 +45,56 @@ module.exports = {
       return;
     }
 
-    const topic = interaction.options.getString("sujet") || "Undefined";
-    const embed = {
-      color: 0x03b894,
-      title: "Nouveaux sondages",
-      description: topic,
-      thumbnail: { url: `${interaction.guild.iconURL({size: 64,})}` },
-      footer: {
-        text: `Sondage créé par ${interaction.user.displayName}`,
-        icon_url: `${interaction.user.displayAvatarURL()}`,
-      },
-    };
+    const response1 = interaction.options.getString("reponse1") || null;
+    const response2 = interaction.options.getString("reponse2") || null;
+    const response3 = interaction.options.getString("reponse3") || null;
 
-    await interaction.reply({ embeds: [embed] });
+    const topic = interaction.options.getString("sujet") || "Undefined";
+    const embed = new EmbedBuilder()
+      .setColor(0x03b894)
+      .setTitle("📊 Nouveau sondage")
+      .setDescription(topic)
+      .setThumbnail(interaction.guild.iconURL({ size: 64 }))
+      .setFooter({
+        text: `Sondage créé par ${interaction.user.username}`,
+        iconURL: interaction.user.displayAvatarURL(),
+      });
+
+    embed.fields = [];
+    if (response1) {
+      embed.fields.push({
+        name: "1️⃣ Option 1",
+        value: response1,
+        inline: true,
+      });
+    }
+    if (response2) {
+      embed.fields.push({
+        name: "2️⃣ Option 2",
+        value: response2,
+        inline: true,
+      });
+    }
+    if (response3) {
+      embed.fields.push({
+        name: "3️⃣ Option 3",
+        value: response3,
+        inline: true,
+      });
+    }
+
+    const pollMessage = await interaction.reply({
+      embeds: [embed],
+      fetchReply: true,
+    });
+
+    if (response1 != null || response2 != null || response3 != null) {
+      if (response1) pollMessage.react("1️⃣");
+      if (response2) pollMessage.react("2️⃣");
+      if (response3) pollMessage.react("3️⃣");
+    } else {
+      pollMessage.react("✅");
+      pollMessage.react("❌");
+    }
   },
 };
